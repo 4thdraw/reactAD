19b269fbff5fb4b497d28423bc6e2a544354bab9
4thdraw <4thdraw2@gmail.com>
2023-01-16 21:57:55 +0900

    배너제작해야함

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Navi from './component/Navi';
+import About from './component/About';
+import Maincomponent from './component/Maincomponent';
+
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navi></Navi>
+      <Routes>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/' element={<Maincomponent></Maincomponent>}></Route>
+      </Routes>     
     </div>
   );
 }
