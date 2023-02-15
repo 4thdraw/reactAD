a37cd85c4d449c36b83949d8c40182e20cd63ee9
4thdraw <4thdraw2@gmail.com>
2023-02-15 12:00:53 +0900

    메뉴활성화는 단지 리액트훅 사용하지않고 스타일추가로 처리하기

--- src/component/Navi.js
+++ src/component/Navi.js
@@ -3,27 +3,18 @@ import logo from '../logo.svg';
 import navicss from '../css/navi.module.css';
 import { Link } from 'react-router-dom';
 import Scrollspy from 'react-scrollspy';
+
 // npm i react-router-dom
 // npm install react-bootstrap bootstra
 // npm i react-scrollspy
 
  function Navi() {
     const [ colorMode, updataColor ] = useState(true);
-    const [ activenum , naviUpdate ] = useState(-1);
+    const menulist = [['메뉴1','#faq'],['후기','#review'],['묻고답하기','#qna']];
 
-    // const boxs = document.querySelectorAll(".navilist a"); //li들
+  
+  
 
-    // boxs.forEach(el => {
-    //   el.onclick = (e) => {
-    //     const nodes = [...e.target.parentElement.children];
-    
-    //     console.log(e.target.parentElement, nodes);
-    
-    //     const index = nodes.indexOf(e.target);
-    
-    //     console.log(index)
-    //   }
-    // });
 
 
   return (
@@ -39,37 +30,31 @@ import Scrollspy from 'react-scrollspy';
             </Link>
         </h1>
         <Scrollspy className='d-flex'>
-            <li key='navi0' data-num={0} className={ 'navilist' }  >
-                <Link to="/" onClick={ 
-                    (e) => {
+            {
+                menulist.map((val, idx) => {
+                    return(
+                        <li key={`navi${idx}`} className='navilist' onClick={
+                            (e) => {
+                                menulist.forEach((v, i)=>{
+                                    if( i === idx ) {
+                                       document.getElementsByClassName('navilist').item(i).classList = 'navilist on';
+                                    }else{
+                                       document.getElementsByClassName('navilist').item(i).classList = 'navilist';
+                                    }
+                                })
+                                
+                            }
+                         }>
+                            <a href={val[1]}>{val[0]}</a>
+                        </li>
 
-                  
-                  const navinodes = [...e.target.parentElement.parentElement.children];
-                  //index 번호 색출하기위함
-                  console.log(navinodes.length);
+                    )
+                })
+                
+            }
 
-                  const index = navinodes.indexOf(e.target.parentElement);
-                  console.log(index)
-                  
-                         
-
-
-                          } }
-                >뇌새김소개</Link>
-            </li>
-            <li key='navi1' data-num={1} className={ 'navilist' }>
-                <a href='#faq' onClick={ 
-                    (e) => {
-                            console.log(e.target.parentNode.parentNode); //스펠링주의할것
-                             
-                          } }>뇌새김의 학습원리</a>
-            </li>
-            <li key='navi2' data-num={2} className={ 'navilist' }>
-                <a href='#review'>뇌새김의 후기</a>
-            </li>
-            <li key='navi3' data-num={3} className={ 'navilist' }>
-                <a href='#qna'>문의하기</a>
-            </li>         
+          
+         
         </Scrollspy>
             <ul id="sns" className='d-flex'>
                 <li>
